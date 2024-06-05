4f786b11061915f5bea3d11ad6a1a00fec0de986
MohitY70 <mohit.yadav@shinedezign.com>
2024-06-05 06:00:04 +0000

    google ads api

--- app/Services/GoogleAdsService.php
+++ app/Services/GoogleAdsService.php
@@ -17,15 +17,15 @@ class GoogleAdsService
     public function __construct()
     {
         $oAuth2Credential = (new OAuth2TokenBuilder())
-            ->withClientId('304351325631-nd7eg4gu95mlo88cc6fps15e9l75aik1.apps.googleusercontent.com')
-            ->withClientSecret('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')
-            ->withRefreshToken('1//05KcT-9ntcRdjCgYIARAAGAUSNwF-L9Ir5GG3hN5bQu5zkuXEb9i9aS5Wf6jKNfcsJi7FQyE6lGXZmkdHg33h2zpxkax2E_wzj7I')
+            ->withClientId('')
+            ->withClientSecret('')
+            ->withRefreshToken('')
             ->build();
 
         $this->client = (new GoogleAdsClientBuilder())
-            ->withDeveloperToken('ZEEd2ZULXzdAsDnHPrqF4g')
+            ->withDeveloperToken('')
             ->withOAuth2Credential($oAuth2Credential)
-            ->withLoginCustomerId('9663882815')
+            ->withLoginCustomerId('')
             ->build();
     }
 
@@ -37,7 +37,7 @@ class GoogleAdsService
         try {
             $response = $keywordPlanIdeaServiceClient->generateKeywordHistoricalMetrics(
                 new GenerateKeywordHistoricalMetricsRequest([
-                    'customer_id' => '9663882815',
+                    'customer_id' => '',
                     'keywords' => $keywords,
                     'geo_target_constants' => [ResourceNames::forGeoTargetConstant(2840)],
                     'keyword_plan_network' => KeywordPlanNetwork::GOOGLE_SEARCH,
