63823ccba22e4ac989461ca734f05888b400659b
MohitY70 <mohit.yadav@shinedezign.com>
2024-07-11 12:50:13 +0000

    design fix

--- resources/views/website/create.blade.php
+++ resources/views/website/create.blade.php
@@ -15,29 +15,50 @@
                         </div>
                     @endif
 
+                    <style>
+                        .fieldset-gray {
+                            background-color: #f7f7f7;
+                            padding: 10px;
+                            border-radius: 5px;
+                            margin-bottom: 15px;
+                        }
+                        .fieldset-gray legend {
+                            font-weight: bold;
+                        }
+                    </style>
+
                     <form method="POST" action="{{ route('admin.websites.store') }}">
                         @csrf
 
                         <x-form-field label="Name" name="name" value="{{ old('name') }}" required="true" />
                         <x-form-field label="Select property type" name="property_type" :options="['domain' => 'Domain', 'url_prefix' => 'URL prefix']" required="true" />
                         <x-form-field label="Domain/URL" name="url" type="text" value="{{ old('url') }}" required="true" />
-                        <x-form-field label="Google Analytics Client ID" name="GOOGLE_ANALYTICS_CLIENT_ID" value="{{ old('GOOGLE_ANALYTICS_CLIENT_ID') }}" />
-                        <x-form-field label="Google Analytics Client Secret" name="GOOGLE_ANALYTICS_CLIENT_SECRET" value="{{ old('GOOGLE_ANALYTICS_CLIENT_SECRET') }}" />
-                        <x-form-field label="Google Analytics Redirect URI" name="GOOGLE_ANALYTICS_REDIRECT_URI" type="text" value="{{ old('GOOGLE_ANALYTICS_REDIRECT_URI') }}" />
-                        <x-form-field label="API Key" name="API_KEY" value="{{ old('API_KEY') }}" />
-                        <x-form-field label="Google Ads Developer Token" name="GOOGLE_ADS_DEVELOPER_TOKEN" value="{{ old('GOOGLE_ADS_DEVELOPER_TOKEN') }}" />
-                        <x-form-field label="Google Ads Client ID" name="GOOGLE_ADS_CLIENT_ID" value="{{ old('GOOGLE_ADS_CLIENT_ID') }}" />
-                        <x-form-field label="Google Ads Client Secret" name="GOOGLE_ADS_CLIENT_SECRET" value="{{ old('GOOGLE_ADS_CLIENT_SECRET') }}" />
-                        <x-form-field label="Google Ads Redirect URI" name="GOOGLE_ADS_REDIRECT_URI" type="text" value="{{ old('GOOGLE_ADS_REDIRECT_URI') }}" />
-                        <x-form-field label="Google Ads Key" name="GOOGLE_ADS_KEY" value="{{ old('GOOGLE_ADS_KEY') }}" />
-                        <x-form-field label="Google Ads Login Customer ID" name="GOOGLE_ADS_LOGIN_CUSTOMER_ID" value="{{ old('GOOGLE_ADS_LOGIN_CUSTOMER_ID') }}" />
+
+                        <fieldset class="fieldset-gray">
+                            <legend>Google Analytics Settings</legend>
+                            <x-form-field label="Google Analytics Client ID" name="GOOGLE_ANALYTICS_CLIENT_ID" value="{{ old('GOOGLE_ANALYTICS_CLIENT_ID') }}" />
+                            <x-form-field label="Google Analytics Client Secret" name="GOOGLE_ANALYTICS_CLIENT_SECRET" value="{{ old('GOOGLE_ANALYTICS_CLIENT_SECRET') }}" />
+                            <x-form-field label="Google Analytics Redirect URI" name="GOOGLE_ANALYTICS_REDIRECT_URI" type="text" value="{{ old('GOOGLE_ANALYTICS_REDIRECT_URI') }}" />
+                            <x-form-field label="API Key" name="API_KEY" value="{{ old('API_KEY') }}" />
+                        </fieldset>
+
+                        <fieldset class="fieldset-gray">
+                            <legend>Google Ads Settings</legend>
+                            <x-form-field label="Google Ads Developer Token" name="GOOGLE_ADS_DEVELOPER_TOKEN" value="{{ old('GOOGLE_ADS_DEVELOPER_TOKEN') }}" />
+                            <x-form-field label="Google Ads Client ID" name="GOOGLE_ADS_CLIENT_ID" value="{{ old('GOOGLE_ADS_CLIENT_ID') }}" />
+                            <x-form-field label="Google Ads Client Secret" name="GOOGLE_ADS_CLIENT_SECRET" value="{{ old('GOOGLE_ADS_CLIENT_SECRET') }}" />
+                            <x-form-field label="Google Ads Redirect URI" name="GOOGLE_ADS_REDIRECT_URI" type="text" value="{{ old('GOOGLE_ADS_REDIRECT_URI') }}" />
+                            <x-form-field label="Google Ads Key" name="GOOGLE_ADS_KEY" value="{{ old('GOOGLE_ADS_KEY') }}" />
+                            <x-form-field label="Google Ads Login Customer ID" name="GOOGLE_ADS_LOGIN_CUSTOMER_ID" value="{{ old('GOOGLE_ADS_LOGIN_CUSTOMER_ID') }}" />
+                        </fieldset>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     Add Project
                                 </button>
-                                <a href="{{ url()->previous() }}" class="btn btn-secondary" >
-                                        {{ __('Back') }}
+                                <a href="{{ url()->previous() }}" class="btn btn-secondary">
+                                    {{ __('Back') }}
                                 </a>
                             </div>
                         </div>
