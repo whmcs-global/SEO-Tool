9d9f758a1871a06ebdc1e0092589cd4d304a97a7
Mohit Yadav <149043648+AnmolShineDezign@users.noreply.github.com>
2024-11-15 13:15:48 +0530

    Update new_dashboard.blade.php

--- resources/views/new_dashboard.blade.php
+++ resources/views/new_dashboard.blade.php
@@ -154,7 +154,7 @@
 
                             <div class="col-12 col-md-4">
                                 <div class="card" style="margin-bottom: 0px !important">
-                                    <div class="m-2">
+                                    <div class="m-1">
                                         <h5 class="card-title">Last 28 Days</h5>
                                         <div class="row">
                                             <div class="col-6">
@@ -175,7 +175,7 @@
                             </div>
                             <div class="col-12 col-md-4">
                                 <div class="card" style="margin-bottom: 0px !important">
-                                    <div class="m-2">
+                                    <div class="m-1">
                                         <h5 class="card-title">Last 7 Days</h5>
                                         <div class="row">
                                             <div class="col-6">
@@ -198,7 +198,7 @@
 
                             <div class="col-12 col-md-4">
                                 <div class="card" style="margin-bottom: 0px !important">
-                                    <div class="m-2">
+                                    <div class="m-1">
                                         <h5 class="card-title">Yesterday</h5>
                                         <div class="row">
                                             <div class="col-6">
